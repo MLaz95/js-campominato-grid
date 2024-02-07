887a776787cf4a6daf045ce19050f0a2e00ebbd5
marco <lazzari.marco.ml@gmail.com>
2024-02-07 17:08:28 +0100

    aggiunta selezione difficoltá; bonus completato

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const gridElement = document.querySelector('#grid');
+const difficultyElement = document.querySelector('#difficulty');
 const buttonElement = document.querySelector('#button');
 
 buttonElement.addEventListener('click', function(){
-
+    // reset
     gridElement.innerHTML = '';
 
-    for(i = 1; i <= 100; i++){
+    // records grid size from difficulty selector
+    let gridSize = difficultyElement.value;
+
+    // adds cells till the grid size limit is reached
+    for(i = 1; i <= gridSize; i++){
         const newCell = document.createElement('div');
         newCell.classList.add('cell');
+
+        // adds a different class to determine the size of each cell depending on grid size
+        if (gridSize == 49){
+            newCell.classList.add('easy');
+        } else if (gridSize == 81){
+            newCell.classList.add('medium');
+        } else {
+            newCell.classList.add('hard');
+        }
+
         newCell.innerHTML = i;
 
+        // marks a cell as active and logs its number
         newCell.addEventListener('click', function(){
             newCell.classList.add('active');
             console.log(newCell.innerHTML);

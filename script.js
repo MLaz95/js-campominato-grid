f8f832e995df08f570c136090a26340061215923
marco <lazzari.marco.ml@gmail.com>
2024-02-07 16:14:07 +0100

    ogni cella risponde al click

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const buttonElement = document.querySelector('#button');
 buttonElement.addEventListener('click', function(){
 
     gridElement.innerHTML = '';
-    
+
     for(i = 1; i <= 100; i++){
         const newCell = document.createElement('div');
         newCell.classList.add('cell');
         newCell.innerHTML = i;
+
+        newCell.addEventListener('click', function(){
+            newCell.classList.add('active');
+            console.log(newCell.innerHTML);
+        });
+
         gridElement.append(newCell);
     }
 })
